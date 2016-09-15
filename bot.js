725d7a4375aee08f1e1b2985fc7561565fba70e8
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2016-09-15 19:31:26 +0100

    Added get_request

--- bot.js
+++ bot.js
@@ -1,1 +1,18 @@
+function get_hot_questions(quantity) {
+    return new Promise(function(resolve, reject) {
+        
+    });
+}
 
+function get_request(url, responsetype, encoding) {
+    return new Promise(function(resolve, reject) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.addEventListener("error", reject);
+        xmlhttp.addEventListener("abort", reject);
+        xmlhttp.addEventListener("load", resolve);
+        xmlhttp.open(url, "GET");
+        xmlhttp.responseType = responsetype;
+        xmlhttpsetRequestHeaders("accept-encoding", encoding);
+        xmlhttp.send();
+    });
+}
