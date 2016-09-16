a1c286acd5c7c99cea0d95a67b096b48a58db723
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2016-09-16 08:11:46 +0100

    Added request code to get_hot_questions

--- bot.js
+++ bot.js
@@ -1,7 +1,11 @@
-function get_hot_questions(quantity) {
-    return new Promise(function(resolve, reject) {
+function get_hot_questions(pagesize, page) {
+    return get_request(
+        "https://api.stackexchange.com/2.2/questions?page=" + page +
+        "&pagesize=" + pagesize +
+        "&order=desc&sort=hot&site=retrocomputing"
+    ).then(function(resolve, reject) {
         
-    });
+    })
 }
 
 function get_request(url, responsetype) {
@@ -11,7 +15,9 @@ function get_request(url, responsetype) {
         xmlhttp.addEventListener("abort", reject);
         xmlhttp.addEventListener("load", resolve);
         xmlhttp.open(url, "GET");
-        xmlhttp.responseType = responsetype;
+        if (bool !== undefined) {
+            xmlhttp.responseType = responsetype;
+        }
         xmlhttp.send();
     });
 }
