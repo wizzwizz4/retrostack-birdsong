12cb561329a80cc5b2b701510c27f98f21439b26
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2016-09-16 19:11:51 +0100

    Add backoff to get_hot_questions

--- bot.js
+++ bot.js
@@ -1,12 +1,23 @@
 function get_hot_questions(pagesize, page) {
+    if (get_hot_questions.backoff) {
+        throw "backoff";
+    }
     return get_request(
         "https://api.stackexchange.com/2.2/questions?page=" + page +
         "&pagesize=" + pagesize +
         "&order=desc&sort=hot&site=retrocomputing"
-    ).then(function(resolve, reject) {
-        
+    ).then(function(response) {
+        qobj = JSON.parse(response.responseText);
+        if (qobj.hasOwnProperty("backoff")) {
+            get_hot_questions.backoff = true;
+            setTimeout(function(){
+                get_hot_questions.backoff = true;
+            }, qobj.backoff * 1000)
+        }
+        return qobj; //Do some more pre-processing first!
     })
 }
+get_hot_questions.backoff = false;
 
 function get_request(url, responsetype) {
     return new Promise(function(resolve, reject) {
