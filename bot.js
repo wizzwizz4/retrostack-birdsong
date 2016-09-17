f10e2c75cf172ab253183fbc27ae4d4335d98ce5
wizzwizz4 <wizzwizz4@users.noreply.github.com>
2016-09-17 10:38:29 +0100

    Added get_twitter_timeline_fragments

--- bot.js
+++ bot.js
@@ -14,10 +14,29 @@ function get_hot_questions(pagesize, page) {
                 get_hot_questions.backoff = true;
             }, qobj.backoff * 1000)
         }
-        return qobj; //Do some more pre-processing first!
+        return qobj;
     })
 }
 get_hot_questions.backoff = false;
+
+function get_twitter_timeline_fragments(userid, count, maxid, fragments) {
+    if (fragments === undefined) {
+        fragments = [];
+    }
+    return get_request(
+        "https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=" + userid +
+        "&count=" + ((count > 200) ? 200 : count) +
+        ((maxid === undefined) ? "" : ("&max_id=" + maxid))
+    ).then(function(response) {
+        qobj = JSON.parse(response.responseText);
+        fragments.concat(qobj);
+        if (fragments.length > count) {
+            return fragments;
+        } else {
+            return get_twitter_timeline_fragments(userid, count, qobj[qobj.length - 1].id_str, fragments);
+        }
+    });
+}
 
 function get_request(url, responsetype) {
     return new Promise(function(resolve, reject) {
